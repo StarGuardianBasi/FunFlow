9f4a4d09abd8f06392811c5e53d71319402cfa09
StarGuardianBasi <basiliclezard@gmail.com>
2018-10-25 15:55:36 +0200

    Creating the front end of the main activity, now waiting for the ability to create intents for the grid

---
+++ app/src/main/java/fr/utt/if26/funflow/ItemSheet.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.funflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_sheet);
+    }
+}
